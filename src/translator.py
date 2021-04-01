60b153a8914a98f9e84be38e9b74eab41d7fe314
lucachiossi <engr.luca.chiossi@gmail.com>
2021-04-01 18:48:47 +0200

    translator functions implemented and tested succesfully

--- src/translator.py
+++ src/translator.py
@@ -33,3 +33,28 @@ def english_to_french(english_text):
         model_id='en-fr').get_result()
     french_text = translation['translations'][0]['translation']
     return french_text
+
+def english_to_german(english_text):
+    """
+    this function translates english text to
+    german text using IBM WLT service
+    """
+    # get service authentication credentials
+    load_dotenv(find_dotenv())
+    api_key = os.getenv('API_KEY')
+    api_url = os.getenv('API_URL')
+
+    # authenticate with cloud service
+    authenticator = IAMAuthenticator(api_key)
+    language_translator = LanguageTranslatorV3(
+        version='2018-05-01',
+        authenticator=authenticator
+        )
+    language_translator.set_service_url(api_url)
+
+    # return text translation
+    translation = language_translator.translate(
+        text=english_text,
+        model_id='en-de').get_result()
+    german_text = translation['translations'][0]['translation']
+    return german_text
--- tests/test_translator.py
+++ tests/test_translator.py
@@ -3,7 +3,7 @@ test_translator.py
 """
 
 import unittest
-from src.translator import english_to_french
+from src.translator import english_to_french, english_to_german
 
 class TestTranslator(unittest.TestCase):
     """
@@ -16,3 +16,11 @@ class TestTranslator(unittest.TestCase):
         self.assertEqual(english_to_french('Hello my friend'), 'Bonjour mon ami')
         self.assertEqual(english_to_french('Help, please'), "Aide, s'il vous plaît")
         self.assertEqual(english_to_french('It is time to code'), 'Il est temps de coder')
+
+    def test_english_to_german(self):
+        """
+        test function english_to_german
+        """
+        self.assertEqual(english_to_german('Hello my friend'), 'Hallo mein Freund')
+        self.assertEqual(english_to_german('Help, please'), 'Hilfe, bitte.')
+        self.assertEqual(english_to_german('It is time to code'), 'Es ist Zeit zum Code')
